7eb51c085cb4c3be4ba84a2806755593724137ff
humdrum <jjrajani@gmail.com>
2017-09-22 10:36:37 -0400

    console for errors

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
+        console.log(__dirname);
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
